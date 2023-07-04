e15fe2259030afdf0db0da528c292782919c40d9
lynxsole <john1880@outlook.fr>
2023-07-04 13:21:52 +0200

    finition du carousel

---
+++ index.js
@@ -0,0 +1,46 @@
+let s1 = 0;
+
+function slideR() {
+    s1 += 1;
+    console.log('slide', s1);
+
+   switch (s1) {
+    case s1 = 1:
+        document.getElementById('carousselPosition').style.marginLeft = "-0";
+        break;
+
+    case s1 = 2:
+        document.getElementById('carousselPosition').style.marginLeft = "-2000px";
+        break;
+
+    case s1 = 3:
+        document.getElementById('carousselPosition').style.marginLeft = "-4000px";
+    break;
+   
+    default:
+        break;
+   }
+}
+
+function slideL() {
+    s1 -= 1;
+    console.log('slide', s1);
+
+    switch (s1) {
+        case s1 = 1:
+            document.getElementById('carousselPosition').style.marginLeft = "-0";
+            break;
+    
+        case s1 = 2:
+            document.getElementById('carousselPosition').style.marginLeft = "-2000px";
+            break;
+    
+        case s1 = 3:
+            document.getElementById('carousselPosition').style.marginLeft = "-4000px";
+        break;
+       
+        default:
+            break;
+       }
+
+}
